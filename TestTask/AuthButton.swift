5aa5097d7ea9e0f3530ae2e09f5bcaa918f4ca3e
Karim Sadykov <103373439+Skarim333@users.noreply.github.com>
2023-03-10 13:03:27 +0300

    add AuthButton

--- TestTask/AuthButton.swift
+++ TestTask/AuthButton.swift
@@ -5,4 +5,56 @@
 //  Created by Карим Садыков on 10.03.2023.
 //
 
-import Foundation
+import UIKit
+
+class AuthButton: UIButton {
+    
+    enum ButtonType {
+        case signIn
+        case signUp
+        case plain
+
+        var title: String {
+            switch self {
+            case .signIn: return "Sign In"
+            case .signUp: return "Sign Up"
+            case .plain: return "-"
+            }
+        }
+    }
+
+    let type: ButtonType
+
+    init(type: ButtonType, title: String?) {
+        self.type = type
+        super.init(frame: .zero)
+        if let title = title {
+            setTitle(title, for: .normal)
+        }
+        configure()
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError()
+    }
+
+    private func configure() {
+        if type != .plain {
+            setTitle(type.title, for: .normal)
+        }
+        switch type {
+        case .signIn:
+            backgroundColor = .systemBlue
+            setTitleColor(.white, for: .normal)
+        case .signUp:
+            backgroundColor = .clear
+            setTitleColor(.blue, for: .normal)
+        case .plain:
+            setTitleColor(.link, for: .normal)
+            backgroundColor = .clear
+        }
+        titleLabel?.font = .systemFont(ofSize: 18, weight: .semibold)
+        layer.cornerRadius = 8
+        layer.masksToBounds = true
+    }
+}
--- TestTask/SignInViewController.swift
+++ TestTask/SignInViewController.swift
@@ -18,11 +18,15 @@ final class SignInViewController: UIViewController, UITextFieldDelegate {
     private let lastNameField = AuthField(type: .lastName)
     private let emailField = AuthField(type: .email)
     
+    private let signInButton = AuthButton(type: .signIn, title: "Sign In")
+    private let logInButton = AuthButton(type: .signUp, title: "Log In")
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
         addSubviews()
         configureFields()
+        configureButtons()
     }
     
     private func addSubviews() {
@@ -30,6 +34,17 @@ final class SignInViewController: UIViewController, UITextFieldDelegate {
         view.addSubview(firstNameField)
         view.addSubview(lastNameField)
         view.addSubview(emailField)
+        view.addSubview(signInButton)
+        view.addSubview(logInButton)
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        firstNameField.frame = CGRect(x: 44, y: 259, width: 289, height: 29)
+        lastNameField.frame = CGRect(x: 44, y: 323, width: 289, height: 29)
+        emailField.frame = CGRect(x: 44, y: 387, width: 289, height: 29)
+        signInButton.frame = CGRect(x: 43, y: 451, width: 289, height: 46)
+        logInButton.frame = CGRect(x: 173.6, y: 514.43, width: 28, height: 9.57)
     }
     
     func configureFields() {
@@ -54,10 +69,51 @@ final class SignInViewController: UIViewController, UITextFieldDelegate {
         emailField.resignFirstResponder()
     }
     
-    override func viewDidLayoutSubviews() {
-        super.viewDidLayoutSubviews()
-        firstNameField.frame = CGRect(x: 44, y: 259, width: 289, height: 29)
-        lastNameField.frame = CGRect(x: 44, y: 323, width: 289, height: 29)
-        emailField.frame = CGRect(x: 44, y: 387, width: 289, height: 29)
+    func configureButtons() {
+        signInButton.addTarget(self, action: #selector(didTapSignIn), for: .touchUpInside)
+        logInButton.addTarget(self, action: #selector(didTapLogIn), for: .touchUpInside)
+    }
+    
+    
+    @objc func didTapSignIn() {
+        didTapKeyboardDone()
+
+//        guard let email = emailField.text,
+//              let password = passwordField.text,
+//              !email.trimmingCharacters(in: .whitespaces).isEmpty,
+//              !password.trimmingCharacters(in: .whitespaces).isEmpty,
+//              password.count >= 6 else {
+//
+//            let alert = UIAlertController(title: "Woops", message: "Please enter a valid email and password to sign in.", preferredStyle: .alert)
+//            alert.addAction(UIAlertAction(title: "Dismiss", style: .cancel, handler: nil))
+//            present(alert, animated: true)
+//            return
+//        }
+//
+//        AuthManager.shared.signIn(with: email, password: password) { [weak self] result in
+//            DispatchQueue.main.async {
+//                switch result {
+//                case .success:
+//                    HapticsManager.shared.vibrate(for: .success)
+//                    self?.dismiss(animated: true, completion: nil)
+//
+//                case .failure:
+//                    HapticsManager.shared.vibrate(for: .error)
+//                    let alert = UIAlertController(
+//                        title: "Sign In Failed",
+//                        message: "Please check your email and password to try again.", preferredStyle: .alert)
+//                    alert.addAction(UIAlertAction(title: "Dismiss", style: .cancel, handler: nil))
+//                    self?.present(alert, animated: true)
+//                    self?.passwordField.text = nil
+//                }
+//            }
+//        }
+    }
+
+    @objc func didTapLogIn() {
+//        didTapKeyboardDone()
+//        let vc = SignUpViewController()
+//        vc.title = "Create Account"
+//        navigationController?.pushViewController(vc, animated: true)
     }
 }
